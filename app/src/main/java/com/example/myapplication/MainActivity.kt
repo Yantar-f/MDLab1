c7d170fd579e54731f886ec91a90cbe0a5b11315
Матвей Шевченко <shevchenko9542@gmail.com>
2023-10-28 20:10:46 +0600

    structure rough sketch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +26,16 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    LazyColumn {
+                        item { Header() }
+                        item { ProductInfo() }
+                        item { Comments() }
+                        item {
+                            Button(onClick = { /*TODO*/ }) {
+
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -27,17 +43,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Header() {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.header_img),
+            contentDescription = "header image"
+        )
+
+        Row {
+            Text(text = "HEADER")
+        }
+    }
+}
+
+@Composable
+fun ProductInfo() {
+    ProductTags()
+    ProductDescription()
+    VideoList()
+}
+
+@Composable
+fun ProductTags() {
+    
+}
+
+@Composable
+fun VideoList() {
+    
+}
+
+
+@Composable
+fun ProductDescription(text : String = "Dota 2 description text") {
+    Text(text = text)
+}
+
+
+@Composable
+fun Comments(/*commentCards : List<CommentCard> = listOf()*/) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun ProductDescriptionPreview() {
+    ProductDescription();
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+    Header();
 }
