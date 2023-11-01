f3a67482ddb5881e0d817fa250ef6a5dd76b9fef
Матвей Шевченко <shevchenko9542@gmail.com>
2023-11-01 10:35:01 +0600

    feeling of helplessness

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,17 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -26,16 +23,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import com.example.myapplication.entity.Comment
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import java.util.Date
 
@@ -50,21 +48,19 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
+                        val horizontalSidePadding = 15.dp
                         Column {
-                            Header()
-                            ProductInfo()
-                            ReviewSection()
-                        }
+                            Header(horizontalSidePadding)
 
-                        Button(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(150.dp)
-                                .padding(20.dp, 40.dp),
-                            shape = RoundedCornerShape(6.dp),
-                            onClick = { /*TODO*/ }
-                        ) {
-                            Text("Install")
+                            Box(
+                                Modifier.padding(horizontalSidePadding, 0.dp)
+                            ) {
+                                Column {
+                                    ProductInfo()
+                                    ReviewSection()
+                                    InstallButton()
+                                }
+                            }
                         }
                     }
                 }
@@ -74,8 +70,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Header() {
-    val overlayOffset : Float = 100f;
+fun Header(horizontalSidePadding : Dp = 10.dp) {
+    val overlayOffset : Float = 100f
 
     Column {
         Image(
@@ -91,14 +87,15 @@ fun Header() {
             modifier = Modifier
                 .zIndex(2f)
                 .graphicsLayer { translationY = -overlayOffset }
+                .wrapContentSize(unbounded = true)
         ) {
             Image(
                 painter = painterResource(R.drawable.product_logo_img),
                 contentDescription = "logo image",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(100.dp)
-                    .padding(4.dp)
+                    .size(120.dp)
+                    .padding(horizontalSidePadding)
                     .clip(RoundedCornerShape(20.dp))
                     .border(
                         3.dp,
@@ -107,15 +104,24 @@ fun Header() {
                     )
             )
 
-            Column(modifier = Modifier.graphicsLayer { translationY = overlayOffset }) {
+            Column (
+                Modifier
+                    .zIndex(2f)
+                    .graphicsLayer { translationY = overlayOffset }
+            ) {
                 Text(
-                    text = "HEADER"
+                    text = "DoTA 2"
                 )
 
                 HeaderStats()
             }
         }
     }
+}
+
+@Composable
+fun HeaderPicture() {
+
 }
 
 @Preview(showBackground = true)
@@ -151,24 +157,29 @@ fun ProductDescription(text : String = "Dota 2 description text") {
     Text(text = text)
 }
 
+
+@Preview(showBackground = true)
 @Composable
-fun ReviewSection(comments : List<Comment> = listOf(Comment())) {
-    LazyColumn {
-        item { RatingInfo() }
-        items(comments) { comment -> CommentCard(comment) }
+fun ProductDescriptionPreview() {
+    ProductDescription()
+}
+
+@Composable
+fun ReviewSection(comments : List<Comment> = listOf(Comment(), Comment(), Comment())) {
+    Column {
+        RatingInfo()
+        Box {
+            LazyColumn {
+                items(comments) { comment -> CommentCard(comment) }
+            }
+        }
     }
 }
 
 
 @Composable
 fun RatingInfo() {
-
-}
-
-class Comment {
-    var authorName : String = "Author"
-    var date : Date = Date()
-    var text : String = "some comment"
+    Text("Rating info")
 }
 
 @Composable
@@ -195,15 +206,8 @@ fun CommentCard(comment : Comment = Comment()) {
 
         Text(text = comment.text)
     }
-
 }
-/*
 
-@Preview(showBackground = true)
-@Composable
-fun CommentsPreview() {
-    ReviewSection(listOf(Comment(), Comment(), Comment()))
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -213,7 +217,30 @@ fun CommentCardPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun ProductDescriptionPreview() {
-    ProductDescription()
+fun ReviewSectionPreview() {
+    ReviewSection(listOf(Comment(), Comment(), Comment()))
 }
-*/
+
+@Composable
+fun InstallButton() {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(75.dp),
+
+        shape = RoundedCornerShape(10.dp),
+        onClick = { /*TODO*/ }
+    ) {
+        Text(
+            text = "Install",
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InstallButtonPreview() {
+    InstallButton()
+}
+
---
+++ app/src/main/java/com/example/myapplication/entity/Comment.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.entity
+
+import java.util.Date
+
+class Comment {
+    var authorName : String = "Author"
+    var date : Date = Date()
+    var text : String = "some comment"
+}
